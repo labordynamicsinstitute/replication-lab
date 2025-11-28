d561e2ca5070c016c5188851c34eee961afe1ae2
larsvilhuber <lars.vilhuber@cornell.edu>
2025-11-28 14:23:25 +0000

    Deploying to gh-pages from @ labordynamicsinstitute/replication-lab@480f783230207cd70914fd80871c9b02ced40405 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "labordynamicsinstitute/replication-lab-new",
+    "data-repo": "labordynamicsinstitute/replication-lab",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
